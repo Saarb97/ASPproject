c788111d57b145d8119cdd2759a15da8e886df7e
Saar <saarb97@gmail.com>
2021-04-18 23:17:41 +0300

    Add github function, undone modifications to test classes

--- src/test/RunMazeGenerator.java
+++ src/test/RunMazeGenerator.java
@@ -7,10 +7,9 @@ public class RunMazeGenerator {
         testMazeGenerator(new MyMazeGenerator());
     }
     private static void testMazeGenerator(IMazeGenerator mazeGenerator) {
- //prints the time it takes the algorithm to run
-        System.out.println(String.format("Maze generation time(ms): %s", mazeGenerator.measureAlgorithmTimeMillis(100/*rows*/,100/*columns*/)));
-
- //generate another maze
+// prints the time it takes the algorithm to run
+    System.out.println(String.format("Maze generation time(ms): %s", mazeGenerator.measureAlgorithmTimeMillis(100/*rows*/,100/*columns*/)));
+// generate another maze
         Maze maze = mazeGenerator.generate(100/*rows*/, 100/*columns*/);
 // prints the maze
         maze.print();
@@ -18,7 +17,7 @@ public class RunMazeGenerator {
         Position startPosition = maze.getStartPosition();
 // print the start position
         System.out.println(String.format("Start Position: %s", startPosition)); // format "{row,column}"
- //prints the maze exit position
+// prints the maze exit position
         System.out.println(String.format("Goal Position: %s", maze.getGoalPosition()));
     }
 }
--- src/test/RunSearchOnMaze.java
+++ src/test/RunSearchOnMaze.java
@@ -6,54 +6,19 @@ import algorithms.search.*;
 import java.util.ArrayList;
 
 public class RunSearchOnMaze {
-    static boolean x = true;
-
-    public static void main(String[] args) {
-        IMazeGenerator mg = new MyMazeGenerator();
-        while(x) {
-            Maze maze = mg.generate(1000, 1000);
-            SearchableMaze searchableMaze = new SearchableMaze(maze);
-            long startTime = System.currentTimeMillis();
-            solveProblem(searchableMaze, new BreadthFirstSearch()); //BFS
-            long finishTime = System.currentTimeMillis();
-            System.out.print("Time: ");
-            System.out.println(finishTime - startTime);
-            /*
-            startTime = System.currentTimeMillis();
-            solveProblem(searchableMaze, new DepthFirstSearch()); //DFS
-            finishTime = System.currentTimeMillis();
-            System.out.print("Time: ");
-            System.out.println(finishTime - startTime);
-            */
-            startTime = System.currentTimeMillis();
-            solveProblem(searchableMaze, new BestFirstSearch()); //Best
-            finishTime = System.currentTimeMillis();
-            System.out.print("Time: ");
-            System.out.println(finishTime - startTime);
-
-            x = false;
-        }
-
-
+    public static void main(String[] args) { IMazeGenerator mg = new MyMazeGenerator(); Maze maze = mg.generate(30, 30);
+        SearchableMaze searchableMaze = new SearchableMaze(maze);
+        solveProblem(searchableMaze, new BreadthFirstSearch()); solveProblem(searchableMaze, new DepthFirstSearch()); solveProblem(searchableMaze, new BestFirstSearch());
     }
-    private static void solveProblem(ISearchable domain, ISearchingAlgorithm
-            searcher) {
+    private static void solveProblem(ISearchable domain, ISearchingAlgorithm searcher) {
 //Solve a searching problem with a searcher
-        SearchableMaze SM = (SearchableMaze)domain;
-        Solution solution = searcher.solve(domain);
-        if (solution == null) {
-            SM.getMaze().print();
-            System.out.println("------------------------------------------");
-            x = false;
-        }
-        else
-            System.out.println(String.format("'%s' algorithm - nodes evaluated: %s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
+        Solution solution = searcher.solve(domain); System.out.println(String.format("'%s' algorithm - nodes evaluated:  %s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
+
 //Printing Solution Path
-/*
                 System.out.println("Solution path:");
         ArrayList<AState> solutionPath = solution.getSolutionPath();
-        for (int i = 0; i < solutionPath.size(); i++) {
-            System.out.println(String.format("%s. %s",i,solutionPath.get(i)));
-        }*/
+        for (int i = 0; i < solutionPath.size(); i++) { System.out.println(String.format("%s. %s",i,solutionPath.get(i)));
+
+        }
     }
 }
